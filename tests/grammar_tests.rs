325ee434406451b4ac1949b57a5e2b18b30bdec0
Dmytro Kharchenko <dmytriy4565@gmail.com>
2023-11-07 18:18:26 +0100

    added rules and tests

--- tests/grammar_tests.rs
+++ tests/grammar_tests.rs
@@ -16,4 +16,46 @@ fn text_test() -> anyhow::Result<()> {
    
     Ok(())
 }
+#[test]
+fn tag_name_test() -> anyhow::Result<()> {
+    let pair = Grammar::parse(Rule::tag_name, "sometag")?
+        .next()
+        .ok_or_else(|| anyhow!("no pair"))?;
+    assert_eq!(pair.as_str(), "sometag");
+    assert_eq!(pair.as_span().start(), 0);
+    assert_eq!(pair.as_span().end(), 7);
 
+    let pair = Grammar::parse(Rule::tag_name, "1sometag");
+
+    assert!(pair.is_err());
+
+    let pair = Grammar::parse(Rule::tag_name, "_sometag")?
+    .next()
+    .ok_or_else(|| anyhow!("no pair"))?;
+    assert_eq!(pair.as_str(), "_sometag");
+    assert_eq!(pair.as_span().start(), 0);
+    assert_eq!(pair.as_span().end(), 8);
+   
+    Ok(())
+}
+#[test]
+fn attribute_name_test() -> anyhow::Result<()> {
+    let pair = Grammar::parse(Rule::attribute_name, "someattr")?
+        .next()
+        .ok_or_else(|| anyhow!("no pair"))?;
+    assert_eq!(pair.as_str(), "someattr");
+    assert_eq!(pair.as_span().start(), 0);
+    assert_eq!(pair.as_span().end(), 8);
+
+    let pair = Grammar::parse(Rule::attribute_name, "_someattr")?
+    .next()
+    .ok_or_else(|| anyhow!("no pair"))?;
+    assert_eq!(pair.as_str(), "_someattr");
+    assert_eq!(pair.as_span().start(), 0);
+    assert_eq!(pair.as_span().end(), 9);
+
+    let pair = Grammar::parse(Rule::attribute_name, "1someattr");
+    assert!(pair.is_err());
+   
+    Ok(())
+}
