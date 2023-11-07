490fac7862b621bab02e2b46a639d3af160bd338
Dmytro Kharchenko <dmytriy4565@gmail.com>
2023-11-07 15:17:05 +0100

    modified title in readme, started writting grammar and tests

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
-### My Parser
+### My HTML parser
 
-Parser for Rust course
+HTML parser for Rust course
 
 ![Rust Crab](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9jFG4XUD60ap87Arcx6wkBP3GJhYB5ChQyz_1u1onpVI3-4fpshRHDpuV4HE_T5n113E&usqp=CAU)
 
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use my_parser_kma_group_1::*;
 
-use pest::Parser;
 use anyhow::anyhow;
 use my_parser_kma_group_3_Kharchenko::*;
+use pest::Parser;
 
 pub fn main() -> anyhow::Result<()> {
     //println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
 
-    let pair = Grammar::parse(Rule::field, "-273.15")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
+    let pair = Grammar::parse(Rule::text, "-273<.15")?;
+        // .next()
+        // .ok_or_else(|| anyhow!("no pair"))?;
     dbg!(pair);
 
     Ok(())
 }
-
