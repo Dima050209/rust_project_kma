ef6b8af3461c0eaaabf454beb469c6e36ba01ec4
Dmytro Kharchenko <dmytriy4565@gmail.com>
2023-11-07 11:56:16 +0100

    completed practical_lesson

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use my_parser_kma_group_1::*;
 
-pub fn main() {
-    println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
+use pest::Parser;
+use anyhow::anyhow;
+use my_parser_kma_group_3_Kharchenko::*;
+
+pub fn main() -> anyhow::Result<()> {
+    //println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
+
+    let pair = Grammar::parse(Rule::field, "-273.15")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
+    dbg!(pair);
+
+    Ok(())
 }
+
